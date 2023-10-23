4e9b2d1e0f278ab34aed2b78a6a7fc9482d27dfe
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2023-10-23 10:16:31 -0600

    Update to backup original config.

--- Ubuntu-set-StaticIP.sh
+++ Ubuntu-set-StaticIP.sh
@@ -2,11 +2,15 @@
 
 # Define network configuration parameters
 NETWORK_CONFIG_FILE="/etc/netplan/01-netcfg.yaml"
+BACKUP_CONFIG_FILE="/etc/netplan/01-netcfg.yaml.bak"
 INTERFACE_NAME="ens33"  # Change this to your actual interface name
 STATIC_IP="192.168.1.100"  # Change this to your desired static IP address
 NETMASK="255.255.255.0"
 GATEWAY="192.168.1.1"  # Change this to your gateway IP address
 DNS_SERVER="8.8.8.8"  # Change this to your DNS server IP address
+
+# Create a backup of the current Netplan configuration
+sudo cp $NETWORK_CONFIG_FILE $BACKUP_CONFIG_FILE
 
 # Create or modify the Netplan configuration file
 echo "network:
@@ -25,4 +29,4 @@ sudo netplan apply
 # Restart the networking service
 sudo systemctl restart network-manager
 
-echo "Static IP address configuration complete."
+echo "Static IP address configuration complete. Backup saved to $BACKUP_CONFIG_FILE"
