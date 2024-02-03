682e2ce9eecf75c4c41126c32f9a533e64d6c28f
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2024-02-03 07:20:56 -0700

    Code refresh script

---
+++ git-refresh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Note: Copy one level up from script source folder.
+
+# Remove Existing
+rm -dfr ./Linux-Bash-Backup-Scripts/
+
+# Clone from Source
+git clone https://github.com/3n1gmus/Ubuntu-dockerhost-setup-scripts.git
+
+# Set Execution
+chmod +x ./Linux-Bash-Backup-Scripts/*.sh
