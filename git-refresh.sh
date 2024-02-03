96977c48dec1efe715553f778f27a1961c06e57f
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2024-02-03 07:45:24 -0700

    Update

--- git-refresh.sh
+++ git-refresh.sh
@@ -3,10 +3,10 @@
 # Note: Copy one level up from script source folder.
 
 # Remove Existing
-rm -dfr ./Linux-Bash-Backup-Scripts/
+rm -dfr ./Ubuntu-dockerhost-setup-scripts/
 
 # Clone from Source
-git clone https://github.com/3n1gmus/Linux-Bash-Backup-Scripts.git
+git clone https://github.com/3n1gmus/Ubuntu-dockerhost-setup-scripts.git
 
 # Set Execution
-chmod +x ./Linux-Bash-Backup-Scripts/*.sh
+chmod +x ./Ubuntu-dockerhost-setup-scripts/*.sh
