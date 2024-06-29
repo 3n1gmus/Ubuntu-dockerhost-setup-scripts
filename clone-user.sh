6a69149b00afa0546a8622b22e05a83e212611f0
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2024-06-29 07:22:35 -0600

    init

---
+++ clone-user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Usage: clone-user src_user_name new_user_name
+# From: https://blog.yucas.net/2020/11/08/clone-linux-user-copy-user-based-on-another-one/
+
+SRC=$1
+DEST=$2
+
+SRC_GROUPS=$(id -Gn ${SRC} | sed "s/ /,/g" | sed -r 's/\<'${SRC}'\>\b,?//g')
+SRC_SHELL=$(awk -F : -v name=${SRC} '(name == $1) { print $7 }' /etc/passwd)
+
+sudo useradd --groups ${SRC_GROUPS} --shell ${SRC_SHELL} --create-home ${DEST}
+sudo passwd ${DEST}
